57dd400d3428d3b23595e5f6254fe59ecfba898d
unknown <sirilo99x3@gmail.com>
2025-11-04 19:07:01 -0600

    Creacion del Modelo, Contexto y primera migracion

---
+++ BE-CRUDMascotas/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BE_CRUDMascotas.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Mascota> Mascotas { get; set; }
+}
